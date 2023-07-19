26b599621b8c945b4fa50b1b2079fb56dea925cf
nicole-va <nicolevelazque@outlook.com>
2023-07-19 01:47:08 -0500

    25 de mayo

--- server.js
+++ server.js
@@ -1,31 +1,10 @@
 'use strict'
-const express = require('express')
-const app = express()
 
-var api_routes = require('./routes/api');
+var app = require('./app');
+var port = 3700;
 
-//get se usa para solicitar informacion - no manda
-//req parametro trae los datos de la peticion
-//res salida lo que vamos a responder
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
+var server = app.listen(port, () => {
+    console.log("Servidor Corriendo correctamente en la url: http://localhost:"+port);
+});
 
-app.get('/productos', function (req, res) {
-    res.status(200).send({
-        status: 200,
-        message: "Hola mundo"
-    })
-  })
-
-app.get('/precio', function (req, res) {
-    let nom="suma";
-    let n1 = 5;
-    let n2 = 4;
-    res.send('El resultado de la ' + nom + ' es ' + (n1+n2));
-  })
-
-
-console.log("Servidor Corriendo")
-
-app.listen(3700)
+server.timeout = 120000;
