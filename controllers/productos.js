a67e1267d77353df409fc09f4234aee2cf512d3a
nicole-va <nicolevelazque@outlook.com>
2023-07-22 00:24:25 -0500

    15 de junio

--- controllers/productos.js
+++ controllers/productos.js
@@ -1,4 +1,5 @@
 'use strict'
+const { validationResult } = require('express-validator');
 var ProductModel = require('../models/products');
 
 var ProductosController = {
@@ -55,11 +56,112 @@ var ProductosController = {
     },
 
     post_products: (req, res) =>{
-        console.log(req.body)
-        return res.status(200).send({
-            status: 200,
-            data: req.body
+        //validamos los datos que se envian al endpoint
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(422).json({errors: errors.array() });
+        }
+
+        var body = req.body;
+
+        ProductModel.find({sku:body.sku}).then( (data) =>{
+            if(data.length > 0) return res.status(400).send({status: 400, message: "El producto ya existe en la tienda"});
+            
+            var products_model = new ProductModel();
+
+            products_model.sku = body.sku;
+            products_model.name = body.name;
+            products_model.price = body.price;
+            products_model.description = body.description;
+            products_model.image = body.image;
+
+            products_model.save().then( (productsStored) => {
+                    //if(err) return res.status(500).send({message: err});
+            
+                    if(!productsStored) return res.status(404).send({message: "No se pudo almacenar el registro"});
+            
+                   return res.status(200).send({
+                        status: 200,
+                        message: "Producto almacenado."
+                });
+                }).catch((err) => {
+                    return res.status(500).send({message: err});
+                });
+            
+        }).catch( (err) => {
+            return res.status(500).send({status: 500, message: "Error en la busqueda"});
         });
+
+
+        
+//ingresa a la base de datos
+
+        // products_model.save( (err, productsStored) => {
+        //     if(err) return res.status(500).send({message: err});
+
+        //     if(!productsStored) return res.status(404).send({message: "No se pudo almacenar el registro"});
+
+        //     return res.status(200).send({
+        //         status: 200,
+        //         message: "Producto almacenado"
+        //     });
+        // });
+
+
+    },
+
+    delete_products: (req, res) =>{
+        let params = req.params;
+        ProductModel.findOneAndRemove({sku:params.sku}).then((productsStored) =>{
+            if(!productsStored) return res.status(404).send({status: 404, message: "Los datos ingresados son incorrectos"});
+
+            return res.status(200).send({
+                status: 200,
+                message: "Producto eliminado",
+            });    
+        }).catch( (err) => {
+            return res.status(500).send({status: 500, message: "Error al eliminar"});
+        });;
+    },
+
+    update_products: (req, res) =>{
+        //validadmos los datos que se envian el endpoint
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
+        let params= req.params;
+        let products_info = req.body;
+
+        let products_info_update = {
+            sku: products_info.sku,
+            name: products_info.name,
+            price: products_info.price,
+            description: products_info.description,
+            image: products_info.image
+        };
+      
+        ProductModel.findOneAndUpdate(
+          { sku: params.sku },
+          products_info_update,
+          { new: true }
+        )
+          .then((productoUpdate) => {
+            if (!productoUpdate) {
+              return res.status(404).json({ message: 'No existe el producto' });
+            }
+      
+            return res.status(200).json({
+              sku: productoUpdate.sku,
+              name: productoUpdate.name,
+              price: productoUpdate.price,
+              description: productoUpdate.description,
+              image: productoUpdate.image
+            });
+          })
+          .catch((err) => {
+            return res.status(500).json({ message: 'Error al actualizar' });
+          });
     }
 
 
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,28 @@
 'use strict'
 
 var express = require('express');
+var { body } = require('express-validator');
 var api = express.Router();
 var ProductosController = require('../controllers/productos');
 var CategoriasController = require('../controllers/categorias');
 
-api.get('/productos', ProductosController.get_products);
-api.get('/producto/:sku', ProductosController.get_product);
-api.post('/productos', ProductosController.post_products);
+api.get('/productos', ProductosController.get_products);  //todos los productos
+api.get('/producto/:sku', ProductosController.get_product);  //buscar un producto
+api.post('/productos', [
+    body('sku').not().isEmpty(),
+    body('name').not().isEmpty(),
+    body('price').not().isEmpty(),
+    body('description').not().isEmpty(),
+    body('image').not().isEmpty()       // el sku no deberia estar vacio
+],ProductosController.post_products);  //agregar un producto
+api.delete('/productos/:sku', ProductosController.delete_products);
+api.put('/productos/:sku', [
+    //body('sku').not().isEmpty(),
+    body('name').not().isEmpty(),
+    body('price').not().isEmpty(),
+    body('description').not().isEmpty(),
+    body('image').not().isEmpty()       // el sku no deberia estar vacio
+],ProductosController.update_products);   //actualizar un producto
 
 
 api.get('/categorias', CategoriasController.get_categorias);
